aec86adc7c537ce2c49ba0f0af96e90442d2face
cgonguet <cgonguet@db256168-9b6a-11dd-9cf8-e55bc079a4dc>
2009-07-10 08:46:55 +0000

    fix pb when reading very long line

--- trunk/test/com/idea/plugin/traceviewer/core/TracesAnalyzerEngineTest.java
+++ trunk/test/com/idea/plugin/traceviewer/core/TracesAnalyzerEngineTest.java
@@ -1,5 +1,6 @@
 package com.idea.plugin.traceviewer.core;
 
+import com.intellij.execution.ui.ConsoleViewContentType;
 import junit.framework.TestCase;
 
 import java.awt.*;
@@ -7,8 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
-
-import com.intellij.execution.ui.ConsoleViewContentType;
 
 public class TracesAnalyzerEngineTest extends TestCase {
 
@@ -46,6 +45,33 @@ public class TracesAnalyzerEngineTest extends TestCase {
 
   }
 
+  public void testVeryLongLine() throws Exception {
+    String text = "NEW TRACE - Start\n" +
+                  "NEW TRACE - Info 1\n" +
+                  "NEW TRACE - Info 2\n" +
+                  "NEW TRACE - Info 3\n" +
+                  "NEW TRACE - Info 4\n" +
+                  "NEW TRACE - Info 5\n" +
+                  "NEW TRACE - Info 6\n" +
+                  "NEW TRACE - Trace Very Long line: <attributes><aCRpreference>both</aCRpreference><activationInterval>10</activationInterval><activeCallRedirectEnabled>true</activeCallRedirectEnabled><administrativeState>unlocked</administrativeState><auditTime>24</auditTime><auditWindow>10</auditWindow><autoConfigCMDelta>-10</autoConfigCMDelta><autoConfigPWBsrBasedPilotPowerAdjustInterval>120</autoConfigPWBsrBasedPilotPowerAdjustInterval><autoConfigPWIndoorPenetrationLoss>10.0</autoConfigPWIndoorPenetrationLoss><autoConfigPWMaxBSRPowerdBm>13.0</autoConfigPWMaxBSRPowerdBm><autoConfigPWMaxCoverageDistancem>30.0</autoConfigPWMaxCoverageDistancem><autoConfigPWMaxPilotPowerdBm>20.0</autoConfigPWMaxPilotPowerdBm><autoConfigPWMinBSRPowerdBm>0.0</autoConfigPWMinBSRPowerdBm><autoConfigPWMinPilotPowerdBm>-20.0</autoConfigPWMinPilotPowerdBm><autoConfigPWPAdjustmentStepdB>1.0</autoConfigPWPAdjustmentStepdB><autoConfigPWPCPICHPowerIni>-30.0</autoConfigPWPCPICHPowerIni><autoConfigPWPCPICHadjustHysteresisdB>2.0</autoConfigPWPCPICHadjustHysteresisdB><autoConfigPWTargetPilotEcIodB>-14.0</autoConfigPWTargetPilotEcIodB><autoConfigPWTargetPilotRSCPdBm>-100.0</autoConfigPWTargetPilotRSCPdBm><autoConfigPWUeBasedPilotPowerAdjustInterval>120</autoConfigPWUeBasedPilotPowerAdjustInterval><autoConfigProcedureTimer>120</autoConfigProcedureTimer><autoConfigProcedureTimerSqInit>60</autoConfigProcedureTimerSqInit><bCHDecodeGuardTimer>6</bCHDecodeGuardTimer><backhaulClearTimer>210</backhaulClearTimer><bsrPilotPowerAdjustMode>mimBased</bsrPilotPowerAdjustMode><bsrProfileID><unset/></bsrProfileID><cellSearchGuardTimer>6</cellSearchGuardTimer><cipheringEnabled>false</cipheringEnabled><codingScheme>0</codingScheme><dbcMaxRateConvDL>850</dbcMaxRateConvDL><dbcMaxRateConvUL>850</dbcMaxRateConvUL><dbcMaxRateTurboDL>1260</dbcMaxRateTurboDL><dbcMaxRateTurboUL>1260</dbcMaxRateTurboUL><dbcMaxSF>1</dbcMaxSF><defaultPSCEnableFlag>false</defaultPSCEnableFlag><deltPowerReductiondB>3.0</deltPowerReductiondB><enableEthQoS>false</enableEthQoS><enableMTUsizeAdaptation>false</enableMTUsizeAdaptation><enableNormalCallPreemption>true</enableNormalCallPreemption><enableTransportCAC>false</enableTransportCAC><femtoACLenable>true</femtoACLenable><femtoPSCListEnableFlag>false</femtoPSCListEnableFlag><femtoPSCRangeLength>15</femtoPSCRangeLength><femtoPSCReservedIndex>1</femtoPSCReservedIndex><femtoPSCStartRange>0</femtoPSCStartRange><fullnameofNetwork>Paris</fullnameofNetwork><fullnameofNetwork2><unset/></fullnameofNetwork2><hSDPAMeasurementPowerOffset>0.0</hSDPAMeasurementPowerOffset><identityCheckEnabledAutoConfig>true</identityCheckEnabledAutoConfig><interFreqNumCellsMeasReport>1</interFreqNumCellsMeasReport><itfBSRNTPqosMapping>ef</itfBSRNTPqosMapping><itfBSROAMqosMapping>af21</itfBSROAMqosMapping><lMCrouterMACAddressTestLocationWeight>0</lMCrouterMACAddressTestLocationWeight><lMCrouterMACAddressTestMovementWeight>50</lMCrouterMACAddressTestMovementWeight><lMCrouterMACAddressTestTestComplete>false</lMCrouterMACAddressTestTestComplete><lMCrouterMACAddressTestTestScore>0</lMCrouterMACAddressTestTestScore><lMCthresholdLevelLocationWeight>50</lMCthresholdLevelLocationWeight><lMCthresholdLevelMovementWeight>100</lMCthresholdLevelMovementWeight><lMCtraceroutePathTestLocationWeight>0</lMCtraceroutePathTestLocationWeight><lMCtraceroutePathTestMovementWeight>50</lMCtraceroutePathTestMovementWeight><lMCtraceroutePathTestTestComplete>false</lMCtraceroutePathTestTestComplete><lMCtraceroutePathTestTestScore>0</lMCtraceroutePathTestTestScore><lMCvisibleMacroCellsTestLocationWeight>0</lMCvisibleMacroCellsTestLocationWeight><lMCvisibleMacroCellsTestMovementWeight>50</lMCvisibleMacroCellsTestMovementWeight><lMCvisibleMacroCellsTestTestComplete>false</lMCvisibleMacroCellsTestTestComplete><lMCvisibleMacroCellsTestTestScore>0</lMCvisibleMacroCellsTestTestScore><locationName><unset/></locationName><maxSizeHistoryLog>512</maxSizeHistoryLog><nMO>nMOI</nMO><netkListGuardConfigTimer>5000</netkListGuardConfigTimer><oVCFPCPMeasUpd>200</oVCFPCPMeasUpd><oVCFPCPOcfMin>2</oVCFPCPOcfMin><oVCFPCPOvlRedistInt>1</oVCFPCPOvlRedistInt><oVCFPCPOvlRedistMax>2</oVCFPCPOvlRedistMax><oVCFPCPThrCpuFpcpUmts>90</oVCFPCPThrCpuFpcpUmts><overrideLocationCheck>false</overrideLocationCheck><ovsIuCSVoice>125</ovsIuCSVoice><publicUserRejectCause>13</publicUserRejectCause><rABInactivityTimer>120</rABInactivityTimer><reservedRBGWDLBW>0</reservedRBGWDLBW><reservedRBGWULBW>0</reservedRBGWULBW><sampleGaugeCountersTimer>5000</sampleGaugeCountersTimer><selfOptMissedCount>0</selfOptMissedCount><shortnameofNetwork>111</shortnameofNetwork><shortnameofNetwork2><unset/></shortnameofNetwork2><sparePara1><unset/></sparePara1><sparePara2><unset/></sparePara2><sparePara3><unset/></sparePara3><sparePara4><unset/></sparePara4><sparePara5><unset/></sparePara5><tCCOSupervision>60</tCCOSupervision><tHandoverGuard>5</tHandoverGuard><thresholdFactor>20</thresholdFactor><timeZone>0</timeZone><uEtoBSRReportingCriteriaRSSITimetoTrigger>timetotrigger20</uEtoBSRReportingCriteriaRSSITimetoTrigger><uEtoBSRReportingCriteriaReportingInterval>reportinginterval1000</uEtoBSRReportingCriteriaReportingInterval><uTRANDrxCycleCoefficient>3</uTRANDrxCycleCoefficient><ueBasedPilotPowerAdjustMode>disable</ueBasedPilotPowerAdjustMode><umtsNtwkListenEnableFlag>true</umtsNtwkListenEnableFlag><umtsOpenSearchEnableFlag>true</umtsOpenSearchEnableFlag><userLabel><unset/></userLabel></attributes>" +
+                  "suite <attributes><aCRpreference>both</aCRpreference><activationInterval>10</activationInterval><activeCallRedirectEnabled>true</activeCallRedirectEnabled><administrativeState>unlocked</administrativeState><auditTime>24</auditTime><auditWindow>10</auditWindow><autoConfigCMDelta>-10</autoConfigCMDelta><autoConfigPWBsrBasedPilotPowerAdjustInterval>120</autoConfigPWBsrBasedPilotPowerAdjustInterval><autoConfigPWIndoorPenetrationLoss>10.0</autoConfigPWIndoorPenetrationLoss><autoConfigPWMaxBSRPowerdBm>13.0</autoConfigPWMaxBSRPowerdBm><autoConfigPWMaxCoverageDistancem>30.0</autoConfigPWMaxCoverageDistancem><autoConfigPWMaxPilotPowerdBm>20.0</autoConfigPWMaxPilotPowerdBm><autoConfigPWMinBSRPowerdBm>0.0</autoConfigPWMinBSRPowerdBm><autoConfigPWMinPilotPowerdBm>-20.0</autoConfigPWMinPilotPowerdBm><autoConfigPWPAdjustmentStepdB>1.0</autoConfigPWPAdjustmentStepdB><autoConfigPWPCPICHPowerIni>-30.0</autoConfigPWPCPICHPowerIni><autoConfigPWPCPICHadjustHysteresisdB>2.0</autoConfigPWPCPICHadjustHysteresisdB><autoConfigPWTargetPilotEcIodB>-14.0</autoConfigPWTargetPilotEcIodB><autoConfigPWTargetPilotRSCPdBm>-100.0</autoConfigPWTargetPilotRSCPdBm><autoConfigPWUeBasedPilotPowerAdjustInterval>120</autoConfigPWUeBasedPilotPowerAdjustInterval><autoConfigProcedureTimer>120</autoConfigProcedureTimer><autoConfigProcedureTimerSqInit>60</autoConfigProcedureTimerSqInit><bCHDecodeGuardTimer>6</bCHDecodeGuardTimer><backhaulClearTimer>210</backhaulClearTimer><bsrPilotPowerAdjustMode>mimBased</bsrPilotPowerAdjustMode><bsrProfileID><unset/></bsrProfileID><cellSearchGuardTimer>6</cellSearchGuardTimer><cipheringEnabled>false</cipheringEnabled><codingScheme>0</codingScheme><dbcMaxRateConvDL>850</dbcMaxRateConvDL><dbcMaxRateConvUL>850</dbcMaxRateConvUL><dbcMaxRateTurboDL>1260</dbcMaxRateTurboDL><dbcMaxRateTurboUL>1260</dbcMaxRateTurboUL><dbcMaxSF>1</dbcMaxSF><defaultPSCEnableFlag>false</defaultPSCEnableFlag><deltPowerReductiondB>3.0</deltPowerReductiondB><enableEthQoS>false</enableEthQoS><enableMTUsizeAdaptation>false</enableMTUsizeAdaptation><enableNormalCallPreemption>true</enableNormalCallPreemption><enableTransportCAC>false</enableTransportCAC><femtoACLenable>true</femtoACLenable><femtoPSCListEnableFlag>false</femtoPSCListEnableFlag><femtoPSCRangeLength>15</femtoPSCRangeLength><femtoPSCReservedIndex>1</femtoPSCReservedIndex><femtoPSCStartRange>0</femtoPSCStartRange><fullnameofNetwork>Paris</fullnameofNetwork><fullnameofNetwork2><unset/></fullnameofNetwork2><hSDPAMeasurementPowerOffset>0.0</hSDPAMeasurementPowerOffset><identityCheckEnabledAutoConfig>true</identityCheckEnabledAutoConfig><interFreqNumCellsMeasReport>1</interFreqNumCellsMeasReport><itfBSRNTPqosMapping>ef</itfBSRNTPqosMapping><itfBSROAMqosMapping>af21</itfBSROAMqosMapping><lMCrouterMACAddressTestLocationWeight>0</lMCrouterMACAddressTestLocationWeight><lMCrouterMACAddressTestMovementWeight>50</lMCrouterMACAddressTestMovementWeight><lMCrouterMACAddressTestTestComplete>false</lMCrouterMACAddressTestTestComplete><lMCrouterMACAddressTestTestScore>0</lMCrouterMACAddressTestTestScore><lMCthresholdLevelLocationWeight>50</lMCthresholdLevelLocationWeight><lMCthresholdLevelMovementWeight>100</lMCthresholdLevelMovementWeight><lMCtraceroutePathTestLocationWeight>0</lMCtraceroutePathTestLocationWeight><lMCtraceroutePathTestMovementWeight>50</lMCtraceroutePathTestMovementWeight><lMCtraceroutePathTestTestComplete>false</lMCtraceroutePathTestTestComplete><lMCtraceroutePathTestTestScore>0</lMCtraceroutePathTestTestScore><lMCvisibleMacroCellsTestLocationWeight>0</lMCvisibleMacroCellsTestLocationWeight><lMCvisibleMacroCellsTestMovementWeight>50</lMCvisibleMacroCellsTestMovementWeight><lMCvisibleMacroCellsTestTestComplete>false</lMCvisibleMacroCellsTestTestComplete><lMCvisibleMacroCellsTestTestScore>0</lMCvisibleMacroCellsTestTestScore><locationName><unset/></locationName><maxSizeHistoryLog>512</maxSizeHistoryLog><nMO>nMOI</nMO><netkListGuardConfigTimer>5000</netkListGuardConfigTimer><oVCFPCPMeasUpd>200</oVCFPCPMeasUpd><oVCFPCPOcfMin>2</oVCFPCPOcfMin><oVCFPCPOvlRedistInt>1</oVCFPCPOvlRedistInt><oVCFPCPOvlRedistMax>2</oVCFPCPOvlRedistMax><oVCFPCPThrCpuFpcpUmts>90</oVCFPCPThrCpuFpcpUmts><overrideLocationCheck>false</overrideLocationCheck><ovsIuCSVoice>125</ovsIuCSVoice><publicUserRejectCause>13</publicUserRejectCause><rABInactivityTimer>120</rABInactivityTimer><reservedRBGWDLBW>0</reservedRBGWDLBW><reservedRBGWULBW>0</reservedRBGWULBW><sampleGaugeCountersTimer>5000</sampleGaugeCountersTimer><selfOptMissedCount>0</selfOptMissedCount><shortnameofNetwork>111</shortnameofNetwork><shortnameofNetwork2><unset/></shortnameofNetwork2><sparePara1><unset/></sparePara1><sparePara2><unset/></sparePara2><sparePara3><unset/></sparePara3><sparePara4><unset/></sparePara4><sparePara5><unset/></sparePara5><tCCOSupervision>60</tCCOSupervision><tHandoverGuard>5</tHandoverGuard><thresholdFactor>20</thresholdFactor><timeZone>0</timeZone><uEtoBSRReportingCriteriaRSSITimetoTrigger>timetotrigger20</uEtoBSRReportingCriteriaRSSITimetoTrigger><uEtoBSRReportingCriteriaReportingInterval>reportinginterval1000</uEtoBSRReportingCriteriaReportingInterval><uTRANDrxCycleCoefficient>3</uTRANDrxCycleCoefficient><ueBasedPilotPowerAdjustMode>disable</ueBasedPilotPowerAdjustMode><umtsNtwkListenEnableFlag>true</umtsNtwkListenEnableFlag><umtsOpenSearchEnableFlag>true</umtsOpenSearchEnableFlag><userLabel><unset/></userLabel></attributes>\n" +
+                  "NEW TRACE - End";
+
+    MyEngine engine = new MyEngine(false, false);
+    engine.traceFormat = "NEW TRACE - ";
+    engine.styles = new ArrayList<TraceStyle>();
+    engine.styles.add(new TraceStyle("(Start|End).+", Color.red, Color.WHITE, Font.BOLD, true, false));
+    engine.styles.add(new TraceStyle("Trace.+", Color.black, Color.WHITE, Font.PLAIN, false, true));
+    engine.registerStyles();
+
+    engine.printByBlock = true;
+    BufferedReader reader = new BufferedReader(new StringReader(text));
+    TracesAnalyzerEngine.TracesAnalyzer textLoader = engine.getTracesAnalyzer();
+    textLoader.countLines();
+    while (textLoader.printLine(reader)) {
+    }
+  }
+
   class MyEngine extends TracesAnalyzerEngine {
 
     public MyEngine(boolean resume, boolean filter)
@@ -55,7 +81,7 @@ public class TracesAnalyzerEngineTest extends TestCase {
 
 
     TracesAnalyzer getTracesAnalyzer() {
-      return new TracesAnalyzer(new TraceWorker(){
+      return new TracesAnalyzer(new TraceWorker() {
 
         public void print(String str, ConsoleViewContentType style) {
           System.out.println("PRINT WITH STYLE: " + style);
@@ -65,7 +91,7 @@ public class TracesAnalyzerEngineTest extends TestCase {
         public void ignore(String str) {
           System.out.println("IGNORE: " + str);
         }
-      }, new ProgressIndicatorManager(){
+      }, new ProgressIndicatorManager() {
 
         public void init() {
         }
@@ -77,10 +103,10 @@ public class TracesAnalyzerEngineTest extends TestCase {
         public boolean isCanceled() {
           return false;
         }
-      }){
+      }) {
 
         void countLines() throws IOException {
-           maxLine = 14;
+          maxLine = 14;
         }
       };
     }
@@ -90,15 +116,15 @@ public class TracesAnalyzerEngineTest extends TestCase {
 
   public void testRegexpByBlock() throws Exception {
     String block = "NEW TRACE - INFO - begin here\n" +
-                  "continue and print this: \n" +
-                  "TADA TADA\n" +
-                  "...\n";
+                   "continue and print this: \n" +
+                   "TADA TADA\n" +
+                   "...\n";
 
     String blockline = block.replaceAll("\n", "%BR%");
     System.out.println("blockline = " + blockline);
-    if(blockline.matches("^NEW TRACE - INFO - .+%BR%TADA TADA.*")){
+    if (blockline.matches("^NEW TRACE - INFO - .+%BR%TADA TADA.*")) {
       System.out.println("match");
-    }else{
+    } else {
       System.out.println("no");
     }
   }
