e22bdcd6d165675d5c5e331932593d5b353d4b20
cgonguet <cgonguet@db256168-9b6a-11dd-9cf8-e55bc079a4dc>
2009-11-17 13:17:04 +0000

    v1.2: fix pb when reopens project or when opens in two projects

--- trunk/test/com/idea/plugin/traceviewer/gui/TraceFileChooserTest.java
+++ trunk/test/com/idea/plugin/traceviewer/gui/TraceFileChooserTest.java
@@ -2,17 +2,18 @@ package com.idea.plugin.traceviewer.gui;
 
 import junit.framework.TestCase;
 
+import javax.swing.*;
 import java.io.File;
 
 public class TraceFileChooserTest extends TestCase {
 
   public void testWithDefault() throws Exception {
-    File selectedFile = new TraceFileChooser(new File("tmp/cp_utran.traces")).run();
+    File selectedFile = new TraceFileChooser(new JFrame(),new File("tmp/cp_utran.traces")).run();
     System.out.println("selectedFile = " + selectedFile);
   }
 
   public void testWithDir() throws Exception {
-    File selectedFile = new TraceFileChooser(new File("tmp")).run();
+    File selectedFile = new TraceFileChooser(new JFrame(),new File("tmp")).run();
     System.out.println("selectedFile = " + selectedFile);
   }
 
